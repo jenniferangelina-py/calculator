8d32d257c3cb767f48e1fa96c58dc6e4862150da
jenniferangelina-py <107879448+jenniferangelina-py@users.noreply.github.com>
2022-06-21 13:33:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const operation = document.querySelector('[data-operation]')
-const result = document.querySelector('[data-result]')
-const clear = document.querySelector('[data-clear]')
-const operator = document.querySelectorAll('[data-operator]')
-const number = document.querySelectorAll('[data-number]')
-const equals = document.querySelector('[data-equals]')
+class Calculator {
+  constructor(operation, result) {
+    this.operation = operation;
+    this.result = result;
+    this.clear();
+  }
+  
+  clear() {
+    this.operation = "";
+    this.result = "";
+    this.operator = undefined;
+}
+
+const operation = document.querySelector('[data-operation]');
+const result = document.querySelector('[data-result]');
+const clear = document.querySelector('[data-clear]');
+const operator = document.querySelectorAll('[data-operator]');
+const number = document.querySelectorAll('[data-number]');
+const equals = document.querySelector('[data-equals]');
+
+const Calculator = new Calculator(operation, result);
