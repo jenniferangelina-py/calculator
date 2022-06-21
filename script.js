18ab7212d6cbdfc45367d4f288ff9efc86dda342
jenniferangelina-py <107879448+jenniferangelina-py@users.noreply.github.com>
2022-06-21 11:49:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const operation = document.querySelector('[data-operation]')
+const result = document.querySelector('[data-result]')
+const clear = document.querySelector('[data-clear]')
+const operator = document.querySelectorAll('[data-operator]')
+const number = document.querySelectorAll('[data-number]')
+const number = document.querySelector('[data-equals]')
