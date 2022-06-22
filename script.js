5f37767e82b322886a894312052daef0c5799bb0
jenniferangelina-py <107879448+jenniferangelina-py@users.noreply.github.com>
2022-06-22 21:41:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function appendNumber(number) {
+  document.getElementById("#display").innerHTML = number;
+}
 
+$("#1").click(function(){
+  appendNumber(1);
+});
+
+$("#2").click(function(){
+  appendNumber(2);
+});
+
+$("#3").click(function(){
+  appendNumber(3);
+});
+
+$("#4").click(function(){
+  appendNumber(4);
+});
+
+$("#5").click(function(){
+  appendNumber(5);
+});
+
+$("#6").click(function(){
+  appendNumber(6);
+});
+
+$("#7").click(function(){
+  appendNumber(7);
+});
+
+$("#8").click(function(){
+  appendNumber(8);
+});
+
+$("#9").click(function(){
+  appendNumber(9);
+});
